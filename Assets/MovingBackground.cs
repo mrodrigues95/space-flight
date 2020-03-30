cc3ec1129b5aeffb914ddd41381ceffe64e4950b
mrodrigues95 <38141653+mrodrigues95@users.noreply.github.com>
2020-03-30 12:24:46 -0400

    added moving background

---
+++ Assets/MovingBackground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBackground : MonoBehaviour {
+    public float speed = 1f; // background speed
+    public float clamps;
+    [HideInInspector] public Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start() {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float newPosition = Mathf.Repeat(Time.time * speed, clamps);
+        transform.position = startPosition + Vector3.up * newPosition;
+    }
+}
