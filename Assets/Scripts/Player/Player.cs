5a39565149338bd6bd5ea959dc1d0a1c47a91a14
mrodrigues95 <38141653+mrodrigues95@users.noreply.github.com>
2020-03-31 17:33:20 -0400

    added a health bar

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float moveSpeed = 5f; // player movement speed
+    public Rigidbody2D rb;
+    public int maxHealth = 100;
+    public int currentHealth;
+    public PlayerHealth playerHealth;
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    private void Start() {
+        currentHealth = maxHealth;
+        playerHealth.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        // get player input
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            TakeDamage(20);
+        }
+    }
+
+    // Handle movement
+    private void FixedUpdate() {
+        // move player with a constant move speed
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    // reduce the health value of the player when they take damage
+    private void TakeDamage(int damage) {
+        currentHealth -= damage;
+        playerHealth.SetHealth(currentHealth);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    // set players max health value to 100
+    public void SetMaxHealth(int health) {
+        slider.maxValue = health;
+        slider.value = health;
+        fill.color = gradient.Evaluate(1f); // set health bar color to green
+    }
+
+    // everytime the player takes damage, reduce the slider
+    public void SetHealth(int health) {
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
