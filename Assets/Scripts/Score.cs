4ec23a683e2c313a377e7d37c56fcbe98c1ce91e
mrodrigues95 <38141653+mrodrigues95@users.noreply.github.com>
2020-03-31 18:56:08 -0400

    added a scoring system

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    private int score;
+    private float timer = 0.0f;
+    public Text scoreTextUI;
+
+    // Start is called before the first frame update
+    private void Start() {
+        score = 0;
+    }
+
+    private void Update() {
+        timer += Time.deltaTime;
+        score = (int)(timer % 60);
+        scoreTextUI.text = "Score: " + score.ToString();
+    }
+}
