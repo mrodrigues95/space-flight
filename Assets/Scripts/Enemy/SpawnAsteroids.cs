90d695a0bdb7087efbcc8e339f34afb7079e3301
mrodrigues95 <38141653+mrodrigues95@users.noreply.github.com>
2020-03-31 12:19:40 -0400

    added asteroids and random spawning

---
+++ Assets/Scripts/Enemy/SpawnAsteroids.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAsteroids : MonoBehaviour {
+    public GameObject asteroidPrefab;
+    public float respawnTime = 1.0f;
+
+    // Start is called before the first frame update
+    private void Start() {
+        StartCoroutine(AsteroidWave());
+    }
+
+    private void SpawnAsteroid() {
+        // bottom left most point of the screen
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        // bottom right most point of the screen
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        // add asteroid to the scene
+        GameObject a = Instantiate(asteroidPrefab) as GameObject;
+        a.transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+	}
+    private IEnumerator AsteroidWave() {
+        while (true) {
+            yield return new WaitForSeconds(respawnTime);
+            SpawnAsteroid();
+        }
+    }
+}
