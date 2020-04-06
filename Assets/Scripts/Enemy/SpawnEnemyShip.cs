e9f4bb1bed1c2722828ef848b52056c79480382e
mrodrigues95 <38141653+mrodrigues95@users.noreply.github.com>
2020-04-06 17:47:01 -0400

    added enemy ships

---
+++ Assets/Scripts/Enemy/SpawnEnemyShip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemyShip : MonoBehaviour {
+    public GameObject enemyShipPrefab;
+    public float respawnTime;
+
+    // Start is called before the first frame update
+    private void Start() {
+        StartCoroutine(EnemyShipWave());
+    }
+
+    // Spawn a new enemy ship.
+    private void SpawnShip() {
+        // bottom left most point of the screen
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        // bottom right most point of the screen
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        // add a asteroid to the scene
+        GameObject enemyShip = Instantiate(enemyShipPrefab) as GameObject;
+        enemyShip.transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+        Debug.Log("New enemy ship spawned.");
+    }
+
+    // Call "SpawnShip()" on the respawnTime interval.
+    private IEnumerator EnemyShipWave() {
+        while (true) {
+            yield return new WaitForSeconds(respawnTime);
+            SpawnShip();
+        }
+    }
+}
